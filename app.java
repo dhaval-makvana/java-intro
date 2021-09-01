e28770bbdc1b049a171c21791e73eb42e4392af8
Dhaval Makvana <thedhavalcompany@gmail.com>
2021-09-02 01:08:54 +0530

    chore: strings and variables in java

---
+++ app.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        System.out.println("   /|");
+        System.out.println("  / |");
+        System.out.println(" /  |");
+        System.out.println("/___|");
+
+        String name = "George";
+        int age = 25;
+
+        System.out.println("There was once a man named " + name);
+        System.out.println("he was " + age + " years old. he really liked");
+        System.out.println("the name " + name +" , but didn't like being " + age +".");
+
+        name = "Dhaval";
+        age = 90;
+
+        System.out.println("Most of his friends are also "+ age +", and one");
+        System.out.println("of them was also named " + name);
+    }
+}
